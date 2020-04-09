1b6f32ed498e5cbed7ee43fb1c483c9888fe669d
WarisAli-source <59886087+WarisAli-source@users.noreply.github.com>
2020-04-09 16:33:08 +0530

    complete Program
    Completing the full program of Question Number 20 which assigned to me

--- Question20.c
+++ Question20.c
@@ -1,4 +1,4 @@
-*There are 3 student processes and 1 teacher process. Students are supposed to do their assignments and they need 3 things for that pen, paper and question paper.
+/*There are 3 student processes and 1 teacher process. Students are supposed to do their assignments and they need 3 things for that pen, paper and question paper.
 The teacher has an infinite supply of all the three things. One students has pen,an other has paper and another has question paper. 
 The teacher places two things on a shared table and the student having the third complementary thing makes the assignment and tells the teacher on completion.
 The teacher then places another two things out of the three and again the student having the third thing makes the assignment and tells the teacher on completion.
@@ -33,7 +33,21 @@ int main()
 		int ch1,ch2;
 		printf("\nResources:\n1.pen\n2.paper\n3.question paper\n Enter the two things which is to be placed on the shared table: ");
 		scanf("%d%d",&ch1,&ch2);
-	
+		if(ch1==1 && ch2==2 && s[2].all_three==false)
+		{
+			s[2].all_three=true ;
+			printf("Third Student has completed the task\n");
+		}
+		if(ch1==2 && ch2==3 && s[0].all_three==false)
+		{
+			s[0].all_three=true;
+			printf("First Student has completed the task\n");
+		}
+		if(ch1==1 && ch2==3 && s[1].all_three==false)
+		{
+			s[1].all_three=true;
+			printf("Second Student has completed the task\n");
+		}
 	}
 	printf("All the students now have completed their respective tasks succesfully\n");
 	return 0;
